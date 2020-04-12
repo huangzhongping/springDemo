8f48d6c1ed68190bc104088f2f49a66dbc5a8bdb
黄忠平 <ivan.huang@biposervice.com>
2020-04-12 14:49:28 +0800

    通过localstore 实现用户不刷新页面登录

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -12,10 +12,21 @@ function postClick() {
             "content":content,
             "type":1
         }),
-        contentType: "application/json; charset=utf-8",
+        contentType: "application/json",
         success: function (res) {
             console.log(res)
-            $("#comment-section").hide()
+            if(res.code==200){
+                $("#comment-section").hide()
+            }else if(res.code==2003){
+                    var isAssible = confirm(res.message);
+                    if(isAssible){
+                        window.open("https://github.com/login/oauth/authorize?client_id=9bed93dab6f7fe0d264f&redirect_uri=http://localhost:8887/callback&scope=user&state=123")
+                        window.localStorage.setItem("closeable","true");
+                    }
+            }else{
+                alert("出错了")
+            }
+
         },
         dataType: "json"
     });
